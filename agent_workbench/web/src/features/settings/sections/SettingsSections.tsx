27a1aff50cac8e063705077867c96d3deeaabf85
franknobox <franknobox@qq.com>
2026-05-14 11:31:16 +0800

    refactor(settings): rename component files to shorter names (EngineMCPSection, GeneralSection, ProfileSection)

--- agent_workbench/web/src/features/settings/sections/SettingsSections.tsx
+++ agent_workbench/web/src/features/settings/sections/SettingsSections.tsx
@@ -1,7 +1,7 @@
-// Auto-generated barrel file – import from the individual component files directly.
-export { WORKSPACE_KIND_OPTIONS, GeneralSettingsSection } from "../components/GeneralSettingsSection";
-export { UserProfileSection } from "../components/UserProfileSection";
+// Barrel – re-exports all settings section components from their individual files.
+export { WORKSPACE_KIND_OPTIONS, GeneralSettingsSection } from "../components/GeneralSection";
+export { UserProfileSection } from "../components/ProfileSection";
 export { ToolsSection } from "../components/ToolsSection";
-export { EngineConnectionsSection } from "../components/EngineConnectionsSection";
+export { EngineConnectionsSection } from "../components/EngineMCPSection";
 export { WorkspacesSection } from "../components/WorkspacesSection";
 export { HistorySection } from "../components/HistorySection";
